a304addf2b01d40abfccfd4cbe79f8decdf7c36c
Aradhya Makkar <aradhyamakkar97@gmail.com>
2016-11-01 01:28:20 +0530

    lines thicker and some coloring

--- main.py
+++ main.py
@@ -51,11 +51,15 @@ ttk.Button(root,text="Delete selected group").grid(row=2,column=0)
 ttk.Button(root,text="Expand Selected Group").grid(row=3,column=0)
 ttk.Separator(root,orient=HORIZONTAL).grid(row=4, columnspan=8, sticky="ew")
 ttk.Separator(root,orient=VERTICAL).grid(column=1,row=0, rowspan=8, sticky="ns")
+ttk.Separator(root,orient=VERTICAL).grid(column=2,row=0, rowspan=8, sticky="ns")
+ttk.Separator(root,orient=VERTICAL).grid(column=3,row=0, rowspan=8, sticky="ns")
+ttk.Separator(root,orient=VERTICAL).grid(column=4,row=0, rowspan=8, sticky="ns")
+
 
 
 var = StringVar(root,value="")
 var.set('status of selected group:\n Allegiance selected\n It has 25 ids')
-Label(textvariable=var).grid(row=5)
+Label(textvariable=var,background='green').grid(row=5)
 
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
@@ -64,20 +68,20 @@ Label(textvariable=var).grid(row=5)
 
 # the right side ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 label_1 = Label(text="Enter your message/post here:",height = 5,background='#3b5998',font=(("Helvetica", 16,'bold')) )
-label_1.grid(row=0,column=2,columnspan=5)
+label_1.grid(row=0,column=5,columnspan=5)
 box = Text(root,height=8,width=70,pady=10,padx=10,highlightbackground="black",background="#C8C5C9",font = "Helvetica 20 bold")
-box.grid(row=1,column = 2,columnspan=5)
-ttk.Button(root,text='Message to all friends').grid(row=2,column=2)
-ttk.Button(root,text='Post to the wall').grid(row=2,column=3,ipady=10)
-ttk.Button(root,text='Accept all friend requests').grid(row=2,column=4)
-ttk.Button(root,text='Send friend requests to people you may know').grid(row=3,column=2)
-ttk.Button(root,text='Get information about selected group').grid(row=3,column=4)
+box.grid(row=1,column = 5,columnspan=5)
+ttk.Button(root,text='Message to all friends').grid(row=2,column=5)
+ttk.Button(root,text='Post to the wall').grid(row=2,column=6,ipady=10)
+ttk.Button(root,text='Accept all friend requests').grid(row=2,column=7)
+ttk.Button(root,text='Send friend requests to people you may know').grid(row=3,column=5)
+ttk.Button(root,text='Get information about selected group').grid(row=3,column=7)
 
-status_bar_right = Text(root, height= 8, width= 100)
-status_bar_right.grid(row=5,column= 2,columnspan=5,padx= 5, pady= 10)
+status_bar_right = Text(root, height= 8, width= 100,background='yellow')
+status_bar_right.grid(row=5,column= 5,columnspan=5,padx= 5, pady= 10)
 scrl = Scrollbar(root, command=status_bar_right.yview)
 status_bar_right.config(yscrollcommand=scrl.set)
-scrl.grid(row=5, column=5, sticky='n s')
+scrl.grid(row=5, column=8, sticky='n s')
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
 
