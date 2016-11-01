594049190b4be1fd230ace2459dc5fb2516b11da
Aradhya Makkar <aradhyamakkar97@gmail.com>
2016-11-02 05:27:03 +0530

    using file as db, db.txt has format, get_account function complete

--- main.py
+++ main.py
@@ -2,19 +2,72 @@ from tkinter import *
 from tkinter import messagebox # for displaying messagebox
 from tkinter import ttk # for styling
 from tkinter import Text
-import sqlite3
 
 
-def setup_db():
+f = open('db.txt','a')
+f.close()
+
+f= open('db.txt','r')
+list_of_groups = []
+for line in f:
+    if line[:5]=='GROUP':
+        list_of_groups.append(line[5:-1])
+f.close()
+
+def get_accounts(g):
+    result = []
+    with open('db.txt','r') as ff:
+        to_match = 'GROUP'+g+'\n'
+        semi_result=None
+        antenna = 0
+        for i in ff:
+            if antenna == 1:
+                antenna = 0
+                semi_result = i[0:-1]
+                break
+            if i == to_match:
+                antenna = 1
+        if semi_result == None:
+            return []
+        else:
+            a = semi_result.split(' ')
+            tick = 0
+            t = []
+            for i in a:
+
+                if tick ==0 :
+                    t = []
+                    t.append(i)
+                    tick = 1
+                else:
+                    tick = 0
+                    t.append(i)
+                    result.append(t)
+            return result
+
+def add_new_account():
     pass
 
-def update_group_list():
+def expand_selected_group():
     pass
 
-def load_group():
+def add_new_group():
     pass
 
+def message_to_all_friends():
+    pass
 
+def post_to_the_wall():
+    pass
+
+def accept_friend_requests():
+    pass
+
+def send_friend_requests():
+    pass
+
+def get_info():
+    pass
 
 
 
@@ -24,9 +77,6 @@ def quit_app():
 def show_about(event=None):
     messagebox.showwarning('About','This software is built by Aradhya Makkar.')
 
-
-def new_id_pressed():
-    messagebox.showwarning('Popup','This will be popup for filling up information on the selected group.')
 
 root = Tk()
 root.title("Facebook Multiple Accounts Manager")
@@ -58,7 +108,7 @@ root.configure(background='#3b5998')
 
 
 # the left side ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-add_id_button = ttk.Button(root,text="Add New Account",command=new_id_pressed,width=30)
+add_id_button = ttk.Button(root,text="Add New Account",command=add_new_account,width=30)
 add_id_button.grid(row=0,column=0)
 
 # ........... list box ................
@@ -66,6 +116,8 @@ def trigger(event=None):
     print('triggered!')
     meow = group_list.get(group_list.curselection()[0])
     var.set('status of selected group:\n '+ meow + ' selected\n It has n ids')
+
+
 group_list = Listbox(root,height=15,background="pink")
 group_list.grid(row=1,column=0)
 for item in ["All accounts", "two", "three", "four", "two", "three", "four", "two", "three", "four", "two", "three", "four", "two", "three", "four", "two", "three", "four", "two", "three", "four", "two", "three", "four"]:
@@ -73,12 +125,10 @@ for item in ["All accounts", "two", "three", "four", "two", "three", "four", "tw
 
 group_list.bind('<<ListboxSelect>>',trigger)
 
-#group_list.delete(0, END)
-
 # .............. list box
 
-ttk.Button(root,text="Delete selected group").grid(row=2,column=0)
-ttk.Button(root,text="Expand Selected Group").grid(row=3,column=0)
+ttk.Button(root,text="Expand Selected Group",command=expand_selected_group).grid(row=2,column=0)
+ttk.Button(root,text="Add new group",command=add_new_group).grid(row=3,column=0)
 ttk.Separator(root,orient=HORIZONTAL).grid(row=4, columnspan=8, sticky="ew")
 ttk.Separator(root,orient=VERTICAL).grid(column=1,row=0, rowspan=8, sticky="ns")
 ttk.Separator(root,orient=VERTICAL).grid(column=2,row=0, rowspan=8, sticky="ns")
@@ -99,11 +149,11 @@ label_1 = Label(text="Enter your message/post here:",height = 5,background='#3b5
 label_1.grid(row=0,column=5,columnspan=5)
 box = Text(root,height=8,width=70,pady=10,padx=10,highlightbackground="black",background="light blue",font = "Helvetica 20 bold")
 box.grid(row=1,column = 5,columnspan=5)
-ttk.Button(root,text='Message to all friends').grid(row=2,column=5)
-ttk.Button(root,text='Post to the wall').grid(row=2,column=6,ipady=10)
-ttk.Button(root,text='Accept all friend requests').grid(row=2,column=7)
-ttk.Button(root,text='Send friend requests to people you may know').grid(row=3,column=5)
-ttk.Button(root,text='Get information about selected group').grid(row=3,column=7)
+ttk.Button(root,text='Message to all friends',command = message_to_all_friends).grid(row=2,column=5)
+ttk.Button(root,text='Post to the wall',command= post_to_the_wall).grid(row=2,column=6,ipady=10)
+ttk.Button(root,text='Accept all friend requests',command=accept_friend_requests).grid(row=2,column=7)
+ttk.Button(root,text='Send friend requests to people you may know',command=send_friend_requests).grid(row=3,column=5)
+ttk.Button(root,text='Get information about selected group',command=get_info).grid(row=3,column=7)
 
 status_bar_right = Text(root, height= 8, width= 100,background='#C8C5C9')
 status_bar_right.grid(row=5,column= 5,columnspan=5,padx= 5, pady= 10)
